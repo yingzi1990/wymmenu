3ce548815999b14abbf23622e4e5650e4327d52b
master <php-dev@qq.com>
2024-09-07 17:14:45 +0800

    ddsfdsf

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 import os
 import sys
 from .route import *
+from .config.data import config_path
 
 WEB_DIRECTORY = "./js"
 
+token_name = f"{config_path}/token.txt"
+if not os.path.isfile(token_name):
+    # 文件不存在，创建文件
+    with open(token_name, 'w') as file:
+        file.write('')
